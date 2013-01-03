f9cf8003580c80fce0b67eb6316589c9495d3eca
yamada28go <yamada28go@gmail.com>
2013-01-03 23:00:41 +0900

    regex match

---
+++ include/match_regex.h
@@ -0,0 +1,106 @@
+//
+//--[extension_switch match regex]--
+//
+
+//Copyright [2013] [yamada28go]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#ifndef _EXTENSION_SWITCH_MATCH_REGEX_
+#define _EXTENSION_SWITCH_MATCH_REGEX_
+
+//c++ default
+#include <memory>
+
+// boost 
+#include <boost/regex.hpp>
+
+//extension_switch
+#include "extension_switch.h"
+
+namespace extension_switch
+{
+
+  template < typename CONDITION , typename lamba_type >
+    struct match_regex_holder
+    {
+
+    public:
+	
+      typedef CONDITION CONDITION_TYPE;
+  
+    private:
+
+      CONDITION_TYPE c_;
+      lamba_type l_;
+
+      typedef std::basic_string< typename CONDITION_TYPE::value_type > REGEX_STRING_TYPE;
+      typedef boost::match_results< typename REGEX_STRING_TYPE::const_iterator > MATCH_RESULTS_TYPE;
+      std::unique_ptr< MATCH_RESULTS_TYPE > match_ret_;
+  
+    public:
+      match_regex_holder() = delete;
+      match_regex_holder & operator=( const match_regex_holder & ) = delete;
+
+    match_regex_holder( match_regex_holder && ref ) :
+      c_( ref.c_ ) , l_( ref.l_ ) 
+      {
+	match_ret_ = std::move(ref.match_ret_);
+      }
+	
+    match_regex_holder( const CONDITION_TYPE & c , const lamba_type & l )
+    : c_(c) , l_(l) 
+      {}
+ 
+      auto do_func( const boost::any & r ) -> decltype( l_( *match_ret_ ) ) 
+      {
+	if( NULL != match_ret_.get() )
+	  {
+	    return l_( *match_ret_ );
+	  }
+	throw std::runtime_error("Match results pointer was null.");
+      }
+
+      template < typename JUDGMENT_TYPE >
+      bool is_match( boost::any r , typename std::enable_if< std::is_same< JUDGMENT_TYPE , REGEX_STRING_TYPE  >::value >::type* = 0) 
+      {
+	const REGEX_STRING_TYPE & str = *(boost::any_cast< const REGEX_STRING_TYPE * >( r ));
+	match_ret_.reset( new MATCH_RESULTS_TYPE() );
+	if( true ==  boost::regex_match( str , *match_ret_ , c_ ) )
+	  {
+	    return true;
+	  }
+	return false;
+
+      }
+
+      template < typename JUDGMENT_TYPE >
+      bool is_match( boost::any r , typename std::enable_if< !std::is_same< JUDGMENT_TYPE , REGEX_STRING_TYPE >::value >::type* = 0) 
+      {
+	return false;
+      }
+
+    };
+
+  namespace match
+  {
+    template < typename CONDITION , typename lamba_type >
+      match_regex_holder< CONDITION , lamba_type > regex( const CONDITION & cond , const lamba_type & t )
+      {
+	return std::move( match_regex_holder< CONDITION , lamba_type >( cond , t ) );
+      }
+  }
+
+}
+
+#endif // end of _EXTENSION_SWITCH_MATCH_REGEX_
---
+++ test/test3.cpp
@@ -0,0 +1,71 @@
+//Copyright [2013] [yamada28go]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+
+//Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#include <iostream>
+
+#include "extension_switch.h"
+#include "match_regex.h"
+
+//! test3 : match regex
+
+namespace {
+
+  void verification( const std::string & line )
+  { 
+    using namespace extension_switch;
+    _switch
+      ( line ,
+	match::regex< boost::regex >(boost::regex( "^[0-9]+$" ), 
+				     []( const boost::smatch & result )
+				     { 
+				       std::cout << "This line is digits only." << std::endl;
+				     }) ,
+	// match::regex< boost::regex >( boost::regex("^[a-z]+$") , 
+	// 			     []( const boost::smatch & result )
+	// 			     { 
+	// 			       std::cout << "This line is lower case." << std::endl;
+	// 			     }),
+	// match::regex< boost::regex >( boost::regex("^[A-Z]+$") , 
+	// 			     []( const boost::smatch & result )
+	// 			     { 
+	// 			       std::cout << "This line is uppercase." << std::endl;
+	// 			     }),
+	// match::regex< boost::regex >( boost::regex("^This year is (\\d+).$") , 
+	// 			     []( const boost::smatch & result )
+	// 			     { 
+	// 			       std::cout << "Year string is " << result.str(1) << "."  << std::endl;
+	// 			     }),
+	match::other( []( const boost::any & ref)
+		      {
+			std::cout << "default type" << std::endl;
+		      })
+	);
+  }
+}
+
+int main()
+{
+  const std::vector< std:: string > line = 
+    {"123",
+     "abc",
+     "ABC",
+    "This year is 2013."};
+  for(const auto & l : line)
+    {
+      std::cout << "src  : " << l  << std::endl;
+      verification(l);
+    }
+}
+
+
